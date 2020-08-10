37c2709c20bdb6c8f6af06b7cf91da35a2da266c
David Zhang <david8zhang@berkeley.edu>
2020-08-09 19:21:17 -0700

    initial commit

---
+++ Assets/Scripts/Bug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Bug", menuName = "Bug")]
+public class Bug : ScriptableObject
+{
+    public string bugName;
+    public int attack;
+    public int defense;
+    public int health;
+    public int speed;
+    public int experience;
+    public int level;
+    public Sprite image;
+}
